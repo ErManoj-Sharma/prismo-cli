8bb947536a871a30b8b8cc12ba69aa10f6c85265
manojsharma <mmmanojsharma201611@gmail.com>
2025-11-26 04:02:43 +0000

    add relations

--- src/utils/prismaUtil.js
+++ src/utils/prismaUtil.js
@@ -18,12 +18,7 @@ function addModelToPrisma(name, fields) {
 
   let schema = fs.readFileSync(schemaPath, "utf8");
 
-  const modelFields = fields
-    .map((f) => {
-      const [fieldName, type] = f.split(":");
-      return `  ${fieldName} ${mapType(type)}`;
-    })
-    .join("\n");
+  const modelFields = generateFieldLines(fields);
 
   const modelBlock = `
 model ${name} {
@@ -33,11 +28,35 @@ ${modelFields}
 }
 `;
 
-  // Append model at the end of file
   schema += "\n" + modelBlock;
   fs.writeFileSync(schemaPath, schema);
 
   console.log(`✨ Model ${name} added to schema.prisma`);
+}
+
+function generateFieldLines(fields) {
+  let relationFields = [];
+  let normalFields = [];
+
+  fields.forEach(f => {
+    const [fieldName, type] = f.split(":");
+
+    if (!type) return;
+
+    if (type === "references" || type === "ref") {
+      const relatedModel = capitalize(fieldName);
+
+      // Main relation field
+      normalFields.push(`  ${fieldName} ${relatedModel} @relation(fields: [${fieldName}Id], references: [id])`);
+      
+      // Foreign key field
+      relationFields.push(`  ${fieldName}Id String`);
+    } else {
+      normalFields.push(`  ${fieldName} ${mapType(type)}`);
+    }
+  });
+
+  return [...normalFields, ...relationFields].join("\n");
 }
 
 function mapType(type) {
@@ -51,10 +70,14 @@ function mapType(type) {
     number: "Int",
     bool: "Boolean",
     boolean: "Boolean",
-    date: "DateTime",
+    date: "DateTime"
   };
 
   return mapping[type.toLowerCase()] || type;
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 module.exports = { addModelToPrisma };
